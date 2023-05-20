ce8dee62516ac6acd34bd1fbc1c086b4977bb03f
Jonathan Marcantonio <lennysgarage1@gmail.com>
2023-05-20 18:56:39 -0400

    refactor: update logging (#5)
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- backend/main.go
+++ backend/main.go
@@ -11,25 +11,36 @@ import (
 	fetchlist "github.com/lennysgarage/letterboxd-scraper/fetchlist"
 )
 
-func writeList(link string, movies []fetchlist.Movie) {
+func writeList(link string, movies []fetchlist.Movie) error {
 	link = strings.Replace(link, "https://letterboxd.com/", "", 1) // remove url in name of file
+
 	file, err := os.Create(fmt.Sprintf("%s.csv", strings.Replace(link, "/", ":", -1)))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+
 	defer file.Close()
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
+
 	headers := []string{"Title", "LetterboxdURI"}
-	writer.Write(headers)
+	err = writer.Write(headers)
+	if err != nil {
+		return err
+	}
 
 	for _, movie := range movies {
-		writer.Write([]string{movie.Title, movie.Link})
+		err = writer.Write([]string{movie.Title, movie.Link})
+		if err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
 
 func main() {
-	if len(os.Args) == 1 {
+	if len(os.Args) < 2 {
 		fmt.Println("link to letterboxd watchlist or other public list required")
 		os.Exit(1)
 	}
@@ -42,8 +53,14 @@ func main() {
 		go func(link string) {
 			defer wg.Done()
 			movies := fetchlist.FetchWatchlist(link)
-			writeList(link, movies)
+
+			err := writeList(link, movies)
+			if err != nil {
+				log.Fatal("Error writing list, %w", err)
+			}
 		}(link)
 	}
 	wg.Wait()
+
+	log.Println("Finished writing list!")
 }
